119fefdc101d9add7440b04d9348286d1d4d66c8
David Daly <dalyd14@gmail.com>
2021-02-11 09:45:12 -0500

    created template for display test board

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../model')
+const { User, Board } = require('../model')
 
 router.get('/', (req, res) => {
     User.findAll({
@@ -9,6 +9,17 @@ router.get('/', (req, res) => {
         const users = dbUserData.map(user => user.get({ plain: true }))
         res.render('homepage', {
             users
+        })
+    })
+    
+})
+
+router.get('/boards', (req, res) => {
+    Board.findAll()
+    .then(dbBoardData => {
+        const boards = dbBoardData.map(board => board.get({ plain: true }))
+        res.render('board', {
+            boards
         })
     })
     
