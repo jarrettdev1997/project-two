a096798f1e3f9be1ea6dd10dffb3b01632ecfc93
David Daly <dalyd14@gmail.com>
2021-02-11 08:59:00 -0500

    created the user routes for getting and creating users; ready for Jarrett to do log in and sign up

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,4 +13,30 @@ router.get('/', (req, res) => {
     });
 })
 
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: { exclude: ['password']}
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    });
+})
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    });
+})
+
 module.exports = router
