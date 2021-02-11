c227edb82e3a90ccc3fb014fde2b4623efb26700
David Daly <dalyd14@gmail.com>
2021-02-11 09:04:54 -0500

    right now the app is set up to display all signed up users on the homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,16 @@ const router = require('express').Router();
 const { User } = require('../model')
 
 router.get('/', (req, res) => {
-    res.render('homepage')
+    User.findAll({
+        attributes: { exclude: ['password']}
+    })
+    .then(dbUserData => {
+        const users = dbUserData.map(user => user.get({ plain: true }))
+        res.render('homepage', {
+            users
+        })
+    })
+    
 })
 
 module.exports = router
