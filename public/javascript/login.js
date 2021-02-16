d179d40e2b9bdad51732425f074941f6f11843d6
Jarrett Devose <jarrett.dev1997@gmail.com>
2021-02-16 18:14:35 -0500

    login updated/not able to store in session

---
+++ public/javascript/login.js
@@ -0,0 +1,53 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-input').value.trim();
+    //const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-input').value.trim();
+  
+    if (username && password) {
+      await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      }).then((response) => {console.log(response)})
+
+      // check the response status
+    if (response.ok) {
+        console.log('success');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+
+  async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-input').value.trim();
+    const password = document.querySelector('#password-input').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  
